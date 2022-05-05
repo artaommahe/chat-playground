5fccf1d85f86b6875a6aff4f2d9922cd9f942b73
Popov Max <drowpmv@gmail.com>
2022-05-05 16:11:14 +0300

    [react-chat] removed obsolete spec files

--- apps/react-chat/src/app/chat/channel-loader/channel-loader.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { ChannelLoader } from './channel-loader';
-
-describe('Channel loader', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<ChannelLoader />);
-    expect(baseElement).toBeTruthy();
-  });
-});
--- apps/react-chat/src/app/chat/messages-list/messages-list.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { MessagesList } from './messages-list';
-
-describe('MessagesList', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<MessagesList messages={[]} />);
-    expect(baseElement).toBeTruthy();
-  });
-});
